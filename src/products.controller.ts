117c44027c399a3896dc0e14cb7aeee0e486ee04
thiago-rosa <thiagosrosa@gmail.com>
2022-10-23 15:07:17 -0300

    M02-T04

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -8,33 +8,39 @@ import {
 	Delete,
 } from '@nestjs/common';
 
+import { Product } from './product.model';
+
 @Controller('products')
 export class ProductsController {
+	products: Product[] = [
+		new Product('BOOK01', 'Name of the book 1', 29.9),
+		new Product('BOOK01', 'Name of the book 2', 39.9),
+		new Product('BOOK01', 'Name of the book 3', 29.9),
+	];
+
 	@Get()
-	getAll(): string {
-		return 'List all products';
+	getAll(): Product[] {
+		return this.products;
 	}
 
 	@Get(':id')
-	getOne(@Param() params): string {
-		return `List one: ${params.id}`;
+	getOne(@Param() params): Product {
+		return this.products[0];
 	}
 
 	@Post()
-	criar(@Body() product): string {
-		console.log(product);
-		return 'Product created';
+	criar(@Body() product: Product) {
+		product.id = 100;
+		this.products.push(product);
 	}
 
 	@Put()
-	update(@Body() product): string {
-		console.log(product);
-		return 'Product updated';
+	update(@Body() product: Product): Product {
+		return product;
 	}
 
 	@Delete(':id')
-	delete(@Param() params): string {
-		console.log(params);
-		return 'Product deleted';
+	delete(@Param() params) {		
+		this.products.pop();
 	}
 }
